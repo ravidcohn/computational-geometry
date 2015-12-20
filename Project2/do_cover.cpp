71f9c9ef55f40eb82d752ef14d1155c63460ec19
ravid_cohen <ravidcohn@gmail.com>
2015-12-20 20:07:12 +0200

    --

--- Project2/do_cover.cpp
+++ Project2/do_cover.cpp
@@ -16,8 +16,8 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	//Default path for the inputs files.
-	string polygon_path = "polygon4.txt";
-	string camera_path = "þþcamera4.txt";
+	string polygon_path = "þþpolygon.txt";
+	string camera_path = "þþþþþþcamera.txt";
 
 	//Gets the files path from user.
 	switch (argc){
