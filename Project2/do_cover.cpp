62ca5bec559fe5ecf5e2dbaf252a0563cbc0945f
ravid_cohen <ravidcohn@gmail.com>
2015-12-22 18:32:44 +0200

    --

--- Project2/do_cover.cpp
+++ Project2/do_cover.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 {
 	//Default path for the inputs files.
 	string polygon_path = "polygon.txt";
-	string camera_path = "þþcamera.txt";
+	string camera_path = "camera.txt";
 	//Gets the files path from user.
 	switch (argc){
 		case 2:{
